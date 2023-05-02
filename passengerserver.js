994fbbc0cca61fb4f202340940d8558fe39ac7bd
Gomathy Dhanya S <gomathydhanya142@gmail.com>
2023-05-02 15:46:29 +0530

    passenger complete

--- passengerserver.js
+++ passengerserver.js
@@ -2,6 +2,9 @@ const express = require('express')
 const path = require('path');
 const bodyParser = require('body-parser');
 const session = require('express-session');
+const fs = require('fs');
+const shortid = require('shortid');
+
 
 const app = express();
 
@@ -47,5 +50,42 @@ app.get('/announcements', function(req, res) {
         res.sendFile(path.join(__dirname, '/views/index.html'));
         }
     });
+
+    app.post('/addcomplaint', (req, res) => {
+      // extract data from request body
+      const { name, digitalid, type, subject, description } = req.body;
+    
+      // create JSON object from extracted data
+      const complaint = {
+        name,
+        digitalid,
+        type,
+        subject,
+        description,
+        "resolved":false
+      };
+    
+      // read existing data from complaints.json
+      const data = JSON.parse(fs.readFileSync('public/data/complaints.json', 'utf8'));
+
+      const id = shortid.generate().substring(0, 4);
+
+      while (data[id]) {
+        id = shortid.generate().substring(0, 4);
+      }
+    
+      // add new complaint to existing data with the unique ID as the key
+      data['C'+id] = complaint;
+    
+      // write updated data to complaints.json
+      fs.writeFileSync('public/data/complaints.json', JSON.stringify(data));
+    
+      // send response to client
+      res.status(200).sendFile(path.join(__dirname, '/views/passenger/complaintadded.html'));
+    });
+    
+
+
+  
 
 module.exports=app
